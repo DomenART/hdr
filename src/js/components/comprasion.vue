b7b65e80400d420a7e9c368c8f4f4ab5e7443980
Kundius Ruslan <kundius.ruslan@gmail.com>
2017-09-28 10:45:37 +0300

    comprasion start

--- src/js/components/comprasion.vue
+++ src/js/components/comprasion.vue
@@ -1,172 +1,224 @@
 <template>
-	<figure class="cd-image-container">
-		<img src="assets/template/img/slide-1.jpg" alt="Original Image">
-		<span class="cd-image-label" data-type="original">Original</span>
-		
-		<div class="cd-resize-img">
-			<img src="assets/template/img/26566002706_73efdae8e4_o.jpg" alt="Modified Image">
-			<span class="cd-image-label" data-type="modified">Modified</span>
+	<div>
+		<header class="comprasion__header uk-flex uk-flex-between uk-flex-middle">
+			<div class="comprasion__title">Какой стиль интерьера выберешь ты?</div>
+			<a class="comprasion__select" :href="second.url">Выберите свой интерьер</a>
+		</header>
+
+		<div class="comprasion__arrows">
+			<span uk-icon="icon: arrow-left"></span>
+			<span uk-icon="icon: arrow-right"></span>
 		</div>
-		
-		<span class="cd-handle"></span>
-	</figure>
+
+		<figure class="comprasion">
+			<img class="comprasion__main" :src="main.image" alt="">
+			<img class="comprasion__second" :src="second.image" alt="">
+
+			<span class="comprasion__label comprasion__label--main" v-show="!hideMainLabel">{{ main.name }}</span>
+			<span class="comprasion__label comprasion__label--second" v-show="!hideSecondLabel">{{ second.name }}</span>
+
+			<span class="comprasion__handle" @mousedown="down"></span>
+		</figure>
+	</div>
 </template>
 
 <style lang="less" scoped>
-.cd-image-container {
-	position: relative;
-}
-.cd-image-container img {
-	display: block;
+.comprasion__header {
 }
 
-.cd-image-label {
+.comprasion__title {
+	color: #fefefe;
+	font-family: 'Museo Sans', sans-serif;
+	font-size: 24px;
+	font-weight: 500;
+	line-height: 1;
+}
+
+.comprasion__select {
+	color: #cccccc;
+	font-size: 18px;
+	font-weight: 400;
+	line-height: 1;
+}
+
+.comprasion__arrows {
+
+}
+
+.comprasion {
+	position: relative;
+	border-bottom: 1px solid #828281;
+}
+
+.comprasion__main {
+	display: block;
+	position: relative;
+	-webkit-user-select: none;  
+	-moz-user-select: none;    
+	-ms-user-select: none;      
+	user-select: none;
+}
+
+.comprasion__second {
+	position: absolute;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	top: 0;
+	-webkit-user-select: none;  
+	-moz-user-select: none;    
+	-ms-user-select: none;      
+	user-select: none;
+}
+
+.comprasion__label {
 	position: absolute;
 	bottom: 0;
-	right: 0;
-	color: #ffffff;
-	padding: 1em;
-	opacity: 0;
-	transform: translateY(20px);
-	transition: transform 0.3s 0.7s, opacity 0.3s 0.7s;
-	&.is-hidden {
-		visibility: hidden;
+	color: #cccccc;
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 1;
+	padding: 12px;
+	&--main {
+		right: 0;
 	}
-	.is-visible & {
-		opacity: 1;
-		transform: translateY(0);
+	&--second {
+		left: 0;
 	}
 }
 
-.cd-resize-img {
+.comprasion__handle {
 	position: absolute;
-	top: 0;
-	left: 0;
-	width: 0;
+	width: 18px;
 	height: 100%;
-	overflow: hidden;
-	/* Force Hardware Acceleration in WebKit */
-	transform: translateZ(0);
-	-webkit-backface-visibility: hidden;
-	backface-visibility: hidden;
-	img {
+	left: 50%;
+	top: 0;
+	margin-left: -9px;
+	cursor: move;
+	&:hover {
+		&:after {
+			background-color: darken(#d3924d, 10%);
+		}
+	}
+	&:after {
+		content: '';
 		position: absolute;
 		left: 0;
+		top: -15px;
+		width: 100%;
+		height: 18px;
+		border-radius: 2px;
+		background-color: #d3924d;
+	}
+	&:before {
+		content: '';
+		left: 50%;
 		top: 0;
-		display: block;
+		width: 1px;
 		height: 100%;
-		width: auto;
-		max-width: none;
-	}
-	.cd-image-label {
-		right: auto;
-		left: 0;
-	}
-	.is-visible & {
-		width: 50%;
-		/* bounce in animation of the modified image */
-		animation: cd-bounce-in 0.7s;
-	}
-}
-
-@keyframes cd-bounce-in {
-	0% {
-		width: 0;
-	}
-	60% {
-		width: 55%;
-	}
-	100% {
-		width: 50%;
-	}
-}
-.cd-handle {
-	position: absolute;
-	height: 44px;
-	width: 44px;
-	/* center the element */
-	left: 50%;
-	top: 50%;
-	margin-left: -22px;
-	margin-top: -22px;
-	border-radius: 50%;
-	background: #dc717d;
-	cursor: move;
-	box-shadow: 0 0 0 6px rgba(0, 0, 0, 0.2), 0 0 10px rgba(0, 0, 0, 0.6), inset 0 1px 0 rgba(255, 255, 255, 0.3);
-	opacity: 0;
-	transform: translate3d(0, 0, 0) scale(0);
-	&.draggable {
-		/* change background color when element is active */
-		background-color: #445b7c;
-	}
-	.is-visible & {
-		opacity: 1;
-		transform: translate3d(0, 0, 0) scale(1);
-		transition: transform 0.3s 0.7s, opacity 0s 0.7s;
+		background-color: #cccccc;
+		position: absolute;
 	}
 }
 </style>
 
 <script>
 	module.exports = {
-		data: function() {
+		data () {
 			return {
-				greeting: 'Привет2'
+				main: {
+					name: 'Original',
+					image: 'assets/template/img/slide-1.jpg',
+					url: ''
+				},
+				second: {
+					name: 'Modified Image',
+					image: 'assets/template/img/26566002706_73efdae8e4_o.jpg',
+					url: ''
+				},
+				hideMainLabel: false,
+				hideSecondLabel: false
 			}
 		},
 
 		mounted () {
-			// $('.cd-image-container').each(function(){
-			// var actual = $(this);
-			// drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual);
-			// });
-			this.drags($('.cd-handle', this.$el), $('.cd-resize-img', this.$el), this.$el)
+			this.updateClip()
+		},
+
+		computed: {
+			container () {
+				return this.$el.querySelector('.comprasion')
+			},
+
+			mainImage () {
+				return this.container.querySelector('.comprasion__main')
+			},
+
+			secondImage () {
+				return this.container.querySelector('.comprasion__second')
+			},
+
+			mainLabel () {
+				return this.container.querySelector('.comprasion__label--main')
+			},
+
+			secondLabel () {
+				return this.container.querySelector('.comprasion__label--second')
+			},
+
+			mainLabelWidth () {
+				return this.mainLabel.offsetWidth
+			},
+
+			secondLabelWidth () {
+				return this.secondLabel.offsetWidth
+			},
+
+			handle () {
+				return this.container.querySelector('.comprasion__handle')
+			}
 		},
 
 		methods: {
-			drags (dragElement, resizeElement, container) {
-			    dragElement.on("mousedown vmousedown", function(e) {
-			        dragElement.addClass('draggable');
-			        resizeElement.addClass('resizable');
-			 
-			        var dragWidth = dragElement.outerWidth(),
-			            xPosition = dragElement.offset().left + dragWidth - e.pageX,
-			            containerOffset = container.offset().left,
-			            containerWidth = container.outerWidth(),
-			            minLeft = containerOffset + 10,
-			            maxLeft = containerOffset + containerWidth - dragWidth - 10;
-			        
-			        dragElement.parents().on("mousemove vmousemove", function(e) {
-			            leftValue = e.pageX + xPosition - dragWidth;
-			            
-			            //constrain the draggable element to move inside its container
-			            if(leftValue < minLeft ) {
-			                leftValue = minLeft;
-			            } else if ( leftValue > maxLeft) {
-			                leftValue = maxLeft;
-			            }
-			 
-			            widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
-			            
-			            $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
-			                $(this).removeClass('draggable');
-			                resizeElement.removeClass('resizable');
-			            });
-			 
-			            $('.resizable').css('width', widthValue); 
-			 
-			            //function to upadate images label visibility here
-			            // ...
-			            
-			        }).on("mouseup vmouseup", function(e){
-			            dragElement.removeClass('draggable');
-			            resizeElement.removeClass('resizable');
-			        });
-			        e.preventDefault();
-			    }).on("mouseup vmouseup", function(e) {
-			        dragElement.removeClass('draggable');
-			        resizeElement.removeClass('resizable');
-			    });
+			updateClip (coords) {
+				coords = coords || this.clipCoords()
+
+				this.secondImage.style.clip = "rect(0, " + coords.x + "px, " + coords.y + "px, 0)"
+				this.hideMainLabel = coords.x > this.container.offsetWidth - this.mainLabelWidth
+				this.hideSecondLabel = this.secondLabelWidth > coords.x
+			},
+
+			down () {
+	      		document.addEventListener('mousemove', this.move)
+      			document.addEventListener('mouseup', this.up)
+			},
+
+			up () {
+      			document.removeEventListener('mousemove', this.move)
+      			document.removeEventListener('mouseup', this.up)
+			},
+
+			move (e) {
+				let left = e.clientX - this.container.offsetLeft
+
+				if(left < 0) left = 0
+				if(left > this.container.offsetWidth) left = this.container.offsetWidth
+
+				this.handle.style.left = left + 'px'
+
+				this.updateClip({
+					x: left,
+					y: this.mainImage.offsetHeight
+				})
+			},
+
+			clipCoords () {
+				let left = this.handle.getBoundingClientRect().left - this.container.getBoundingClientRect().left
+
+				return {
+					x: left + (this.handle.offsetWidth / 2),
+					y: this.mainImage.offsetHeight
+				}
 			}
 		}
 	}
